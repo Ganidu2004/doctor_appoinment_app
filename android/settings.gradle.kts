b12fbb2a1a9a3454e0f3d47f84b6cc920af376fd
Ganidu2004 <ganiduchalinda@gmail.com>
2026-07-30 20:07:49 +0530

    fix error in debug error💣

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,5 +1,29 @@
-Cardiologist01@gmail.com  = 12345678
+pluginManagement {
+    val flutterSdkPath =
+        run {
+            val properties = java.util.Properties()
+            file("local.properties").inputStream().use { properties.load(it) }
+            val flutterSdkPath = properties.getProperty("flutter.sdk")
+            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            flutterSdkPath
+        }
 
-ganiduchalinda@gmail.com  =  16589000
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
-ganiduuseproject@gmail.com  =  16589000
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services") version("4.4.2") apply false
+    // END: FlutterFire Configuration
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+}
+
+include(":app")
